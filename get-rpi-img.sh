1b748d8a648cdeab1f44b1f0ea0430cb08980329
James Cuzella <james.cuzella@lyraphase.com>
2025-07-20 01:14:16 -0600

    Add helper script to download rasbpian bookworm lite (2025-05-13)

---
+++ get-rpi-img.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-lite.img.xz
+unxz 2025-05-13-raspios-bookworm-arm64-lite.img.xz
