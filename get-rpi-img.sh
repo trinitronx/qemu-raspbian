bbd50b758c238ff1cbfeaf7ca53f045643541bd3
James Cuzella <james.cuzella@lyraphase.com>
2025-07-20 02:11:49 -0600

    get-rpi-img.sh: Set errexit to bail on download or unxz failure

--- get-rpi-img.sh
+++ get-rpi-img.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -o errexit
+
 wget https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-lite.img.xz
 unxz 2025-05-13-raspios-bookworm-arm64-lite.img.xz
