666ec434e21a5df9763b5bbde3f7e51c3024c446
James Cuzella <james.cuzella@lyraphase.com>
2025-12-12 17:16:41 -0700

    run-raspi4: Commit testing raspios_full img (works, but no graphics!)

--- .env.mk
+++ .env.mk
@@ -1,7 +1,7 @@
 # /* vim: set ft=make noet : */
 # Raspbian image URL & filename (derived from everything after last '/')
-IMG_URL := https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-lite.img.xz
-#IMG_URL := https://downloads.raspberrypi.org/raspios_full_arm64/images/raspios_full_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-full.img.xz
+#IMG_URL := https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-lite.img.xz
+IMG_URL := https://downloads.raspberrypi.org/raspios_full_arm64/images/raspios_full_arm64-2025-05-13/2025-05-13-raspios-bookworm-arm64-full.img.xz
 IMG := $(subst .xz,,$(notdir $(IMG_URL)))
 
 # Default insecure pi user password: raspberrypiqemu
